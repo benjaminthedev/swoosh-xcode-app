df775e82809dd06428f9219388fd69e87022975d
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-07 15:38:43 +0100

    Initial commit with files

---
+++ swoosh-xcode-app/swoosh-xcode-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh-xcode-app
+//
+//  Created by Benjamin on 07/08/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
