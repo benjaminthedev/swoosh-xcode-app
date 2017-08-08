4b45cd46c10669995ab067afd079df0a36d4db79
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-08 07:44:44 +0100

    Added custom classes for buttons

---
+++ swoosh-xcode-app/swoosh-xcode-app/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  swoosh-xcode-app
+//
+//  Created by Benjamin on 08/08/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
