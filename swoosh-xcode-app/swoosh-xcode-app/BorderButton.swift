623414b14e58a23f36aece34d371c977f8849ebf
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-08 10:09:02 +0100

    Custom button with borders

--- swoosh-xcode-app/swoosh-xcode-app/BorderButton.swift
+++ swoosh-xcode-app/swoosh-xcode-app/BorderButton.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class BorderButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
-    /*New Comment*/
+    //This is for the white border of the button.
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
 
 }
