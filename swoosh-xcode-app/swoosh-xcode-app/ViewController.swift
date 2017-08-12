add5002cd2af045ca2c5d0d8c01fc0ea440d1b8d
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-12 13:51:20 +0100

    new ibOutlets

--- swoosh-xcode-app/swoosh-xcode-app/ViewController.swift
+++ swoosh-xcode-app/swoosh-xcode-app/ViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //test
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
