762b7ed69f9c3c032c6dfa4682ef0bbd4fca0ab9
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-19 08:07:00 +0100

    Updated files to work with ios11 and new xcode

--- swoosh-xcode-app/swoosh-xcode-app/WelcomeVC.swift
+++ swoosh-xcode-app/swoosh-xcode-app/WelcomeVC.swift
@@ -8,13 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var swoosh: UIImageView!
-    
-    @IBOutlet weak var bgImg: UIImageView!
-    
-    
+class WelcomeVC: UIViewController {
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         
