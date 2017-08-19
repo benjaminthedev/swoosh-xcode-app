7d8c9b45f3fc07d3c11093b30a4cd991e3fbf36c
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-19 08:29:16 +0100

    Added more segue interaction  - not from the storyboard but programmatically

--- swoosh-xcode-app/swoosh-xcode-app/LeagueVC.swift
+++ swoosh-xcode-app/swoosh-xcode-app/LeagueVC.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
 
     /*
