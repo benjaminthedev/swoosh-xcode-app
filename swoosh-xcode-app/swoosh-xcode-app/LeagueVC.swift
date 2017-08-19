2873998eba06ef01dba43ec5ab42a7596dd1771d
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-19 11:31:56 +0100

    more files added

--- swoosh-xcode-app/swoosh-xcode-app/LeagueVC.swift
+++ swoosh-xcode-app/swoosh-xcode-app/LeagueVC.swift
@@ -12,23 +12,19 @@ class LeagueVC: UIViewController {
     
     var player: Player!
     
-    
     @IBOutlet weak var nextBtn: BorderButton!
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-    
     @IBAction func onMensTapped(_ sender: Any) {
         selectLeague(leagueType: "mens")
     }
     
-    
     @IBAction func onWomensTapped(_ sender: Any) {
         selectLeague(leagueType: "womens")
     }
-    
     
     @IBAction func onCoedTapped(_ sender: Any) {
         selectLeague(leagueType: "coed")
@@ -46,16 +42,10 @@ class LeagueVC: UIViewController {
     }
 
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
-    */
 
 }
