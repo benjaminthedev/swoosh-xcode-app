f5f2f62562cdac497b0f17454f815b9caf349ded
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-19 10:54:47 +0100

    New files added

---
+++ swoosh-xcode-app/swoosh-xcode-app/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  swoosh-xcode-app
+//
+//  Created by Benjamin on 19/08/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+    
+}
